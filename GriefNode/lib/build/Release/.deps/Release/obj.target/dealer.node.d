8f93df7b46faa2938c28c5e8cf17f1708f5e3688
Kyle Nolan <kcnolan13@gmail.com>
2015-07-18 11:29:03 -0400

    general settings working

---
+++ GriefNode/lib/build/Release/.deps/Release/obj.target/dealer.node.d
@@ -0,0 +1,1 @@
+cmd_Release/obj.target/dealer.node := flock ./Release/linker.lock g++ -shared -pthread -rdynamic -m64  -Wl,-soname=dealer.node -o Release/obj.target/dealer.node -Wl,--start-group Release/obj.target/dealer/dealer_wrap.o -Wl,--end-group 
---
+++ GriefNode/watch_sockets.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+gnome-terminal --window-with-profile=GRIEFER -e 'watch -n0.2 tail -50 /home/kyle/www/html/Griefer/GriefNode/log/sockets.log'
