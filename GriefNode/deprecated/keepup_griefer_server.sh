73f2586003c95fe715ca50b094e5d4d785f6e8f9
root <root@griefserver.griefer.g9.internal.cloudapp.net>
2015-10-16 23:51:13 +0000

    keepalive updates

--- GriefNode/deprecated/keepup_griefer_server.sh
+++ GriefNode/deprecated/keepup_griefer_server.sh
@@ -2,15 +2,19 @@
 
 cd /home/kyle/html/Griefer/GriefNode
 
-case "$(pidof node | wc -w)" in
 
-0|1)  echo "Restarting Griefer:     $(date)" >> /var/log/griefer.txt
-    node griefer.js 2>&1 >> /var/log/griefer.log #sudo gnome-terminal --window-with-profile=GRIEFER -e 'node griefer.js' 
-    node keepup.js 2>&1 >> /var/log/keepup.log
+case "$(ps -e | grep node | wc -w)" in
+
+
+0|1|2|3|4|5|6|7|8|9|10|11|12|13|14|15|16|17|18|19|20)
+	echo "Restarting Griefer:               $(date)" >> /var/log/griefer
+	killall node
+	node griefer.js 2>&1 >> /var/log/griefer #sudo gnome-terminal --window-with-profile=GRIEFER -e 'node griefer.js' 
     ;;
-2)  # all ok
-	echo "All Okay:               $(date)" >> /var/log/griefer.txt
-    ;;
-*)  echo "Too Many Nodes:         $(date)" >> /var/log/griefer.txt
+
+
+*)  
+	# all ok
+	echo "All Okay:               $(date)" >> /var/log/griefer
     ;;
 esac
--- GriefNode/keepup.js
+++ GriefNode/keepup.js
@@ -1,22 +1,64 @@
 var io = require('socket.io-client');
-var socket = io.connect('http://griefer.cloudapp.net:8080',{'connect timeout':5000, 'transports':['websocket']});
+var fs = require('fs');
+var cs = require('console-stamp')(console, 'HH:MM:ss');
 
-console.log("starting keepup utility");
+var target = 'http://griefer.cloudapp.net:8080';
+var arg = process.argv[2];
+if (arg == "puddlesquid") {
+	target = 'http://www.puddlesquid.com:8080';
+}
+
+var socket = io.connect(target,{'connect timeout':5000, 'transports':['websocket']});
+process.stdout.write("\n");
+console.log("starting keepup: "+target);
 
 socket.on("connect",function(){
 	console.log("connected");
 	setInterval(function(){
-		//console.log(".");
+		process.stdout.write(".");
 		socket.emit("PING");
-	}, 30000);
+	}, 10000);
 });
 
+setTimeout(function(){
+	console.log("ending keepup\n");
+	process.exit();
+},50000);
+
 socket.on("disconnect",function(err){
-	console.log("disconnected");
+	console.log("disconnected prematurely\n");
 	if (err)
 		console.log(err);
+	process.exit();
 });
 
 socket.on("PONG", function() {
-	//console.log("p");
+	process.stdout.write("p");
 });
+
+if (arg == "puddlesquid")
+{
+	var log = fs.statSync('/var/log/keepup');
+	var fsize_mb = log["size"]/1000000.0;
+	if (fsize_mb > 5)
+	{
+		console.log("Clearing Keepup Log. Size was: "+fsize_mb+" MB\n\n");
+		log_std = fs.createWriteStream('/var/log/keepup', {flags : 'w'});
+	}
+
+	log = fs.statSync('/var/log/keepup_azure');
+	var fsize_mb = log["size"]/1000000.0;
+	if (fsize_mb > 5)
+	{
+		console.log("Clearing Keepup Log. Size was: "+fsize_mb+" MB\n\n");
+		log_std = fs.createWriteStream('/var/log/keepup', {flags : 'w'});
+	}
+
+	log = fs.statSync('/var/log/griefer');
+	fsize_mb = log["size"]/1000000.0;
+	if (fsize_mb > 5)
+	{
+		console.log("Clearing Griefer Log. Size was: "+fsize_mb+" MB\n\n");
+		log_std = fs.createWriteStream('/var/log/griefer', {flags : 'w'});
+	}
+}
