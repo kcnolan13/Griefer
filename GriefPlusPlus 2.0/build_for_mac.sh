8b13faed0ec86ebecdbc0c479b956f08af0f84db
Kyle Nolan <kylenolan@MM198525-PC.local>
2018-07-09 14:53:25 -0400

    built x86_64 version of GriefPlusPlus.dylib

--- GriefPlusPlus 2.0/build_for_mac.sh
+++ GriefPlusPlus 2.0/build_for_mac.sh
@@ -2,8 +2,9 @@
 # export C_INCLUDE_PATH=boost/
 # export CPLUS_INCLUDE_PATH=boost/
 # export OBJC_INCLUDE_PATH=boost/
-export DYLD_LIBRARY_PATH="Users/kylenolan/source/boost/lib:${DYLD_LIBRARY_PATH}"
-g++ -std=c++11 -stdlib=libc++ -m32 -fPIC -dynamiclib -compatibility_version 1.0 -current_version 1.0.0 -Os -o GriefPlusPlus.dylib -I/Users/kylenolan/Documents/games/griefer/GriefPlusPlus\ 2.0/lib/rapidjson/include -I/Users/kylenolan/Documents/games/griefer/GriefPlusPlus\ 2.0/lib/websocketpp -I/Users/kylenolan/source/boost/include -L/Users/kylenolan/source/boost/lib -lboost_system main.cpp src/*.cpp src/internal/*.cpp #-I "/Users/kylenolan/Documents/games/griefer/GriefPlusPlus 2.0/"
-# g++ -m32 -fPIC -dynamiclib -compatibility_version 1.0 -current_version 1.0.0 -Os *.cpp -o GriefPlusPlus.dylib -I "/Users/kylenolan/Documents/games/griefer/GriefPlusPlus 2.0/" -L -std=c++0x
 
-# -Weverything
+# export DYLD_LIBRARY_PATH="Users/kylenolan/source/boost/lib:${DYLD_LIBRARY_PATH}"
+# g++ -std=c++11 -stdlib=libc++ -m32 -fPIC -dynamiclib -compatibility_version 1.0 -current_version 1.0.0 -Os -o GriefPlusPlus.dylib -I/Users/kylenolan/Documents/games/griefer/GriefPlusPlus\ 2.0/lib/rapidjson/include -I/Users/kylenolan/Documents/games/griefer/GriefPlusPlus\ 2.0/lib/websocketpp -I/Users/kylenolan/source/boost/include -L/Users/kylenolan/source/boost/lib -lboost_system main.cpp src/*.cpp src/internal/*.cpp #-I "/Users/kylenolan/Documents/games/griefer/GriefPlusPlus 2.0/"
+
+export DYLD_LIBRARY_PATH="/opt/local/lib:${DYLD_LIBRARY_PATH}"
+g++ -std=c++11 -stdlib=libc++ -fPIC -dynamiclib -compatibility_version 1.0 -current_version 1.0.0 -Os -o GriefPlusPlus.dylib -I/Users/kylenolan/Documents/games/griefer/GriefPlusPlus\ 2.0/lib/rapidjson/include -I/Users/kylenolan/Documents/games/griefer/GriefPlusPlus\ 2.0/lib/websocketpp -I/opt/local/include -L/opt/local/lib -lboost_system-mt main.cpp src/*.cpp src/internal/*.cpp #-I "/Users/kylenolan/Documents/games/griefer/GriefPlusPlus 2.0/"
